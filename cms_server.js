926dbc6d0023259c0148cb98c291eae66007df7f
SungJin Kim <sungjinkimm@gmail.com>
2020-05-11 18:23:05 -0400

    add new employee complete

--- cms_server.js
+++ cms_server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const chalk = require("chalk")
 
 const connection = (mysql.createConnection = mysql.createConnection({
   host: "localhost",
@@ -16,13 +17,19 @@ connection.connect((err) => {
 });
 
 start = () => {
+  console.log(chalk.yellow.bold("\n Employee Management System \n"))
   inquirer
     .prompt([
       {
         name: "mainQuestion",
         type: "list",
-        message: "Please choose one: ",
-        choices: ["View all employees", "View employees by department"],
+        message: chalk.green("Please choose one: "),
+        choices: [
+          "View all employees",
+          "View employees by department",
+          "Add new employee",
+          chalk.red.bold("EXIT")
+        ],
       },
     ])
     .then((res) => {
@@ -30,7 +37,14 @@ start = () => {
         showAllEmployees();
       }
       if (res.mainQuestion === "View employees by department") {
-        showEmployeeDepartment();
+        showEmployeeByDepartment();
+      }
+      if (res.mainQuestion === "Add new employee") {
+        addNewEmployee();
+      }
+      if(res.mainQuestion === chalk.red.bold("EXIT")) {
+        console.log(chalk.yellow.bold("\n The application has been terminated \n"))
+        connection.end();
       }
     });
 };
@@ -50,7 +64,7 @@ showAllEmployees = () => {
   );
 };
 
-showEmployeeDepartment = () => {
+showEmployeeByDepartment = () => {
   connection.query(`SELECT * FROM department`, (err, results) => {
     if (err) throw err;
 
@@ -64,7 +78,7 @@ showEmployeeDepartment = () => {
         {
           name: "departmentName",
           type: "rawlist",
-          message: "Please choose one",
+          message: chalk.green("Please select one"),
           choices: dep,
         },
       ])
@@ -126,6 +140,95 @@ showEmployeeDepartment = () => {
           );
         }
       });
+  });
+};
+
+addNewEmployee = () => {
+  let rol = [];
+  let manager = [];
+
+  connection.query(`SELECT * FROM role`, (err, resRole) => {
+    if (err) throw err;
+
+    resRole.forEach((role) => {
+      rol.push(role.title);
+    });
+
+    connection.query(
+      `SELECT emp.id, emp.first_name, emp.last_name, title Job_Title, name Department, salary Salary, m.first_name Manager_First_Name, m.last_name Manager_Last_Name
+      FROM employee AS emp
+      INNER JOIN role ON emp.role_id = role.id
+      INNER JOIN department ON department_id = department.id
+      LEFT JOIN employee AS m ON m.id = emp.manager_id`,
+      (err, results) => {
+        if (err) throw err;
+        results.forEach((employee) => {
+          // console.table(employee)
+          if (employee.id === 1 || employee.id === 2) {
+            manager.push(employee.first_name + " " + employee.last_name);
+          }
+        });
+
+        inquirer
+          .prompt([
+            {
+              name: "first_name",
+              type: "input",
+              message: chalk.green("Enter employee's First name:"),
+            },
+            {
+              name: "last_name",
+              type: "input",
+              message: chalk.green("Enter employee's Last name:"),
+            },
+            {
+              name: "role",
+              type: "rawlist",
+              message: chalk.green("Select employee's role:"),
+              choices: rol,
+            },
+            {
+              name: "manager",
+              type: "rawlist",
+              message: chalk.green("Select employee's manager:"),
+              choices: manager,
+            },
+          ])
+          .then((answer) => {
+            
+            let roleID;
+            resRole.forEach((role) => {
+              if (answer.role === role.title) {
+                roleID = role.id;
+              }
+            });
+
+            let managerID;
+            results.forEach((employee) => {
+              if ( answer.manager === employee.first_name + " " + employee.last_name) {
+                managerID = employee.id;
+              }
+            });
+
+            connection.query(
+              "INSERT INTO employee SET ?",
+              {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: roleID,
+                manager_id: managerID,
+              },
+              (err) => {
+                if (err) throw err;
+                console.log(
+                  chalk.magenta.bold("\n New employee was successfully added into the database.")
+                );
+                start();
+              }
+            );
+          });
+      }
+    );
   });
 };
 
